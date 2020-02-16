1ca7ffa981090d93321793fdf5882a8840801c18
Jelle Caestecker <jelle.caestecker@student.howest.be>
2020-02-16 18:39:16 +0100

    *Naamgeving GUI in orde gebracht *methodes voor berekenen en weergave *wisselen tussen balk/cilinder verbergt andere grid

--- BerekenInhouden.Wpf/MainWindow.xaml.cs
+++ BerekenInhouden.Wpf/MainWindow.xaml.cs
@@ -28,5 +28,87 @@ namespace BerekenInhouden.Wpf
 
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            VerbergGrids();
+        }
+
+        #region Methods
+        private void VerbergGrids()
+        {
+            grdBalk.Visibility = Visibility.Hidden;
+            grdCilinder.Visibility = Visibility.Hidden;
+        }
+        private void ToonGrid(Grid welkeGrid)
+        {
+            grdHoogte.Visibility = Visibility.Visible;
+            welkeGrid.Visibility = Visibility.Visible;
+            welkeGrid.Margin = new Thickness(10, 133, 0, 0);
+            if (welkeGrid == grdBalk)
+            {
+                grdCilinder.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                grdBalk.Visibility = Visibility.Hidden;
+            }
+        }
+        private void WisInputEnBerekeningen()
+        {
+            txtBreedte.Text = "";
+            txtDiameter.Text = "";
+            txtHoogte.Text = "";
+            txtLengte.Text = "";
+            tbkBerekeningBalk.Text = "";
+            tbkBerekeningCilinder.Text = "";
+        }
+        private int GeefInhoudBalk(int hoogte, int breedte, int lengte)
+        {
+            int inhoudBalk;
+            inhoudBalk = hoogte * breedte * lengte;
+            return inhoudBalk;
+        }
+        private float GeefInhoudCilinder(int hoogte, double diameter)
+        {
+            float inhoudCilinder;
+            inhoudCilinder = (float)(hoogte * Math.Pow((diameter / 2), 2) * Getal_Pi);
+            return inhoudCilinder;
+        }
+        #endregion
+
+        #region EventHandlers
+        private void btnBalk_Click(object sender, RoutedEventArgs e)
+        {
+            ToonGrid(grdBalk);
+            WisInputEnBerekeningen();
+        }
+        private void btnBerekenBalk_Click(object sender, RoutedEventArgs e)
+        {
+            int hoogte;
+            int breedte;
+            int lengte;
+            hoogte = int.Parse(txtHoogte.Text);
+            breedte = int.Parse(txtBreedte.Text);
+            lengte = int.Parse(txtLengte.Text);
+
+            int inhoudBalk = GeefInhoudBalk(hoogte, breedte, lengte);
+            tbkBerekeningBalk.Text = $"De inhoud van de balk is {inhoudBalk}.";
+        }
+        private void btnCilinder_Click(object sender, RoutedEventArgs e)
+        {
+            ToonGrid(grdCilinder);
+            WisInputEnBerekeningen();
+        }
+        private void btnBerekenCilinder_Click(object sender, RoutedEventArgs e)
+        {
+            int hoogte;
+            double diameter;
+            hoogte = int.Parse(txtHoogte.Text);
+            diameter = float.Parse(txtDiameter.Text);
+
+            float inhoudCilinder = GeefInhoudCilinder(hoogte, diameter);
+            tbkBerekeningCilinder.Text = $"De inhoud van de cilinder is {inhoudCilinder}.";
+        }
+        #endregion
     }
 }
